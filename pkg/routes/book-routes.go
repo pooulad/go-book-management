3010244442e2f266534f77e39b3196f5840f1e47
pooulad <86445458+pooulad@users.noreply.github.com>
2023-07-20 16:42:36 +0330

    Update book-routes.go

--- pkg/routes/book-routes.go
+++ pkg/routes/book-routes.go
@@ -1,2 +1,14 @@
 package routes
 
+import (
+	"github.com/gorilla/mux"
+	"github.com/pooulad/go-bookstore/pkg/controllers"
+)
+
+var RegisterBookRoutes = func (router *mux.Router){
+	router.HandleFunc("/book/",controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/book/",controllers.GetBook).Methods("GET")
+	router.HandleFunc("/book/{bookId}",controllers.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{bookId}",controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookId}",controllers.DeleteBook).Methods("DELETE")
+}
