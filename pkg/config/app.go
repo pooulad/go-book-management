ccadecf828c232973646c55dbd71697aa208e4b8
pooulad <86445458+pooulad@users.noreply.github.com>
2023-07-20 16:42:34 +0330

    Update app.go

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,1 +1,19 @@
 package config
+
+import "github.com/jinzhu/gorm"
+
+var (
+	db *gorm.DB
+)
+
+func Connect() {
+	d, err := gorm.Open("mysql", "poulad:username/password?charset=utf8&parseTime=True&loc=Local")
+	if (err) != nil {
+		panic(err)
+	}
+	db = d
+}
+
+func GetDB() {
+	return db
+}
