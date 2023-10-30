46bdcd93f904d9d479183e399a100e5269c2ec0f
pooulad <poooooladi@gmail.com>
2023-10-31 02:27:20 +0330

    complete UpdateBook handler

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -66,3 +66,34 @@ func DeleteBook(w http.ResponseWriter, r *http.Request) {
 	w.Write(res)
 }
 
+func UpdateBook(w http.ResponseWriter, r *http.Request) {
+	UpdateBookStruct := &models.Book{}
+	utils.ParsBody(r, UpdateBookStruct)
+
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		fmt.Println("error while parsing")
+	}
+
+	bookDetails, db := models.GetBookById(ID)
+
+	if UpdateBookStruct.Name != "" {
+		bookDetails.Name = UpdateBookStruct.Name
+	}
+	if UpdateBookStruct.Author != "" {
+		bookDetails.Author = UpdateBookStruct.Author
+	}
+	if UpdateBookStruct.Publication != "" {
+		bookDetails.Publication = UpdateBookStruct.Publication
+	}
+
+	db.Save(&bookDetails)
+	res, _ := json.Marshal(bookDetails)
+	
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
