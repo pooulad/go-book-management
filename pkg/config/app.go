d5514c85e47e2c84efffffa05a1107b02052a6b8
pooulad <poooooladi@gmail.com>
2023-10-30 22:54:28 +0330

    handle connect to db

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -24,3 +24,20 @@ type MysqlConfig struct {
 	Loc       string `json:"loc"`
 }
 
+func Connect() {
+	file, err := os.ReadFile("./config.json")
+	if err != nil {
+		panic(err)
+	}
+
+	var cfg MysqlConfig
+	err = json.Unmarshal(file, &cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	dsn := fmt.Sprintf("user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local")
+	d, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db = d
+}
+
