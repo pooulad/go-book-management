9f4faa6bf1aa4a9ce92b8f5d59f56363b9bc7a61
pooulad <poooooladi@gmail.com>
2023-10-31 02:20:15 +0330

    complete DeleteBook handler

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -48,3 +48,21 @@ func CreateBook(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write(res)
 }
+
+func DeleteBook(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		fmt.Println("error while parsing")
+	}
+
+	bookDetails := models.DeleteBook(ID)
+	res, _ := json.Marshal(bookDetails)
+
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
