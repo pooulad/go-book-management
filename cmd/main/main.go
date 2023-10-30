58995472519c6c1ddc56fea3eb426f940d4f9b7d
pooulad <poooooladi@gmail.com>
2023-10-31 00:22:36 +0330

    setup main file for start web server

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main(){
-	
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/pooulad/go-book-management/pkg/routes"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBoostoreRoutes(r)
+	r.Handle("/", r)
+
+	err := http.ListenAndServe(os.Getenv("PROJECT_HOST"), r)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
