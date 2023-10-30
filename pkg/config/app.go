45b15934b23de2e2712163bb6d39f7e0bc8598b9
pooulad <poooooladi@gmail.com>
2023-10-30 22:53:38 +0330

    create MysqlConfig struct

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,2 +1,13 @@
 package config
 
+type MysqlConfig struct {
+	Username  string `json:"username"`
+	Password  string `json:"password"`
+	Host      string `json:"host"`
+	Name      string `json:"name"`
+	Port      string `json:"port"`
+	Charset   string `json:"charset"`
+	ParseTime string `json:"parseTime"`
+	Loc       string `json:"loc"`
+}
+
