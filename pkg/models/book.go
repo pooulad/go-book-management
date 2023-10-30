7d2a227c09f47a6da4c918e6ece63408172606ee
pooulad <poooooladi@gmail.com>
2023-10-30 23:55:19 +0330

    create and setup Book struct

--- pkg/models/book.go
+++ pkg/models/book.go
@@ -1,2 +1,13 @@
 package models
 
+import "gorm.io/gorm"
+
+var db *gorm.DB
+
+type Book struct {
+	gorm.Model
+	Name string `gorm:"" json:"name"`
+	Author string `json:"author"`
+	Publication string `gorm:"" json:"publication"`
+}
+
