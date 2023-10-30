872b4ff8ce637e519e487ce63cbc0aa2c6890c4c
pooulad <poooooladi@gmail.com>
2023-10-30 23:29:15 +0330

    develop utils and create ParsBody func

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,2 +1,17 @@
 package utils
 
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+func ParsBody(r *http.Request, x interface{}) {
+	body, err := io.ReadAll(r.Body)
+	if err == nil {
+		err = json.Unmarshal([]byte(body), x)
+		if err != nil {
+			return
+		}
+	}
+}
