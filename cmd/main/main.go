11a2c27e2366bb1b889e442e5a44abda9e32a9a5
pooulad <86445458+pooulad@users.noreply.github.com>
2023-10-31 03:39:49 +0330

    read env

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/pooulad/go-book-management/pkg/routes"
+	"github.com/joho/godotenv"
 )
 
 func main() {
@@ -15,8 +16,17 @@ func main() {
 	routes.RegisterBoostoreRoutes(r)
 	r.Handle("/", r)
 
-	hostAddress := fmt.Sprintf("%s:%s", os.Getenv("PROJECT_HOST"), os.Getenv("PROJECT_PORT"))
-	err := http.ListenAndServe(hostAddress, r)
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Some error occured. Err: %s", err)
+	}
+
+	host := os.Getenv("PROJECT_HOST")
+	port := os.Getenv("PROJECT_PORT")
+
+	hostAddress := fmt.Sprintf("%s:%s", host, port)
+	fmt.Println(hostAddress)
+	err = http.ListenAndServe(hostAddress, r)
 	if err != nil {
 		log.Fatal(err)
 	}
